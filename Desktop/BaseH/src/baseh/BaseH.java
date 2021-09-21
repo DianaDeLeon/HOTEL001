cc3a2cb78a3f696fd065f2cf8c03afa269c0a57f
DianaDeLeon <ddeleonr9iumg.edu.gt>
2021-09-21 14:02:12 -0500

    Creacion del proyecto

---
+++ Desktop/BaseH/src/baseh/BaseH.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseh;
+
+/**
+ *
+ * @author Diana De León
+ */
+public class BaseH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
